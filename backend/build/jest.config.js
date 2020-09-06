09e5cf6864c1de11335e63fb5b44d6e76078eaff
Wojciech Matuszewski <wm.matuszewski@wp.pl>
2020-09-06 08:37:14 +0200

    wip

--- backend/build/jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = {
-    testMatch: ["**/*.test.ts"],
-    transform: {
-        "^.+\\.tsx?$": "ts-jest"
-    }
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiamVzdC5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9qZXN0LmNvbmZpZy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBTSxDQUFDLE9BQU8sR0FBRztJQUNmLFNBQVMsRUFBRSxDQUFDLGNBQWMsQ0FBQztJQUMzQixTQUFTLEVBQUU7UUFDVCxhQUFhLEVBQUUsU0FBUztLQUN6QjtDQUNGLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IHtcbiAgdGVzdE1hdGNoOiBbXCIqKi8qLnRlc3QudHNcIl0sXG4gIHRyYW5zZm9ybToge1xuICAgIFwiXi4rXFxcXC50c3g/JFwiOiBcInRzLWplc3RcIlxuICB9XG59O1xuIl19
